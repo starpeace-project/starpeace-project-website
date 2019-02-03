10e24d39ae2a053a4a4b0aff47a0e1fb4c3a9e96
rwampler <rwampler86@msn.com>
2019-02-03 15:20:13 -0800

    attempting to fix auto-generate build

--- components/footer-panel.vue
+++ components/footer-panel.vue
@@ -6,7 +6,7 @@
     .column.is-2
       a(href='/privacy') Privacy
     .column.is-2
-      a(href='https://github.com/starpeace-project/starpeace-project-website', target:'_blank') GitHub
+      a(href='https://github.com/starpeace-project/starpeace-project-website', target='_blank') GitHub
 
   .columns.copyright
     .column.is-6.is-offset-3
